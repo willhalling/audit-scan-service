cf96f4d9622f7aeaa7451144beb461ba15591181
Will Halling <willhalling@gmail.com>
2025-06-28 20:48:13 +0100

    Latest 2

--- dist/routes/lighthouse.routes.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export default router;
-//# sourceMappingURL=lighthouse.routes.d.ts.map
--- dist/services/storage.service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class StorageService {
-    private static getBucket;
-    static uploadScreenshot(buffer: Buffer, auditId: string, type: 'desktop' | 'mobile' | 'annotated-desktop' | 'annotated-mobile', host: string): Promise<string>;
-}
-//# sourceMappingURL=storage.service.d.ts.map
